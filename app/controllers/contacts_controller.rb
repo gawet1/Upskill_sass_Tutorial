77b3d76cad2d2dc149263d93efbda3ebcfe1ff24
Tana Gawe <gawet1@tcnj.edu>
2021-03-01 19:18:20 +0000

    Added contact database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,25 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
-
+  
+  def create
+    @contact = Contact.new(contact_paramas)
+    if @contact.save
+      redirect_to new_contact_path, notice:"Message Sent."
+    else
+      redirect_to new_contact_path, notice:"Error Occured."
+    end
+  end
+  
+  
+  private
+    def contact_paramas
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
+
+  
+  
+
+
